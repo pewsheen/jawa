a5203505644d07bec7a131b4838b327381865af4
Wu Yu Wei <yuweiwu@pm.me>
2026-02-07 18:49:55 +0900

    feat: add QLabel

---
+++ crates/cxx-qt-widgets/src/qlabel.rs
@@ -0,0 +1,42 @@
+use crate::WidgetPtr;
+
+pub use ffi::QLabel;
+
+#[cxx_qt::bridge]
+mod ffi {
+    unsafe extern "C++" {
+        include!("cxx-qt-lib/qstring.h");
+        type QString = cxx_qt_lib::QString;
+    }
+
+    unsafe extern "C++Qt" {
+        include!(<QtWidgets/QLabel>);
+
+        /// Display text or an image.
+        #[qobject]
+        type QLabel;
+
+        /// Set the text shown on the label.
+        #[cxx_name = "setText"]
+        fn set_text(self: Pin<&mut QLabel>, text: &QString);
+
+        /// Shows the widget and its child widgets.
+        fn show(self: Pin<&mut QLabel>);
+    }
+
+    #[namespace = "rust::cxxqtlib1"]
+    unsafe extern "C++Qt" {
+        include!("cxx-qt-lib/common.h");
+
+        #[doc(hidden)]
+        #[cxx_name = "make_unique"]
+        fn new_label() -> UniquePtr<QLabel>;
+    }
+}
+
+impl ffi::QLabel {
+    /// Creates a new label without a parent.
+    pub fn new() -> WidgetPtr<Self> {
+        ffi::new_label().into()
+    }
+}
